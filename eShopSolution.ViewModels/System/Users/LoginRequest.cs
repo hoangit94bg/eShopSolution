e3b10b8172bf10df653649c9a1f6c28810270d5f
hoangit94bg <hoangit94bg@gmail.com>
2020-04-18 00:55:07 +0700

    Add fluent validation

--- eShopSolution.ViewModels/System/Users/LoginRequest.cs
+++ eShopSolution.ViewModels/System/Users/LoginRequest.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace eShopSolution.ViewModels.System.Users
 {
     public class LoginRequest
     {
+        [Required(ErrorMessage ="User name is required")]
         public string UserName { get; set; }
 
         public string Password { get; set; }
